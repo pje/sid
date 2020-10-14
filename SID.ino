b6deade184b8d8bf0dcfd5d375b25317a9be2830
Patrick Ellis <patrick.j.ellis@gmail.com>
2020-10-13 23:26:52 -0400

    fix the thing where we were sharp by 2.5 cents
    also add some comments

--- SID.ino
+++ SID.ino
@@ -206,103 +206,107 @@ const double sid_decay_and_release_values_to_seconds[16] = {
 
 int num_presets = 0;
 
+// there's apparently not widespread agreement on which frequency midi notes
+// represent. What we have below is "scientific pitch notation". Ableton, maxmsp
+// and garageband use C3, which is shifted an octave lower.
+// https://en.wikipedia.org/wiki/Scientific_pitch_notation#See_also
 const double MIDI_NOTES_TO_FREQUENCIES[96] = {
-  16.351597831287414,
-  17.323914436054505,
-  18.354047994837977,
-  19.445436482630058,
-  20.601722307054366,
-  21.826764464562746,
-  23.12465141947715,
-  24.499714748859326,
-  25.956543598746574,
-  27.5,
-  29.13523509488062,
-  30.86770632850775,
-  32.70319566257483,
-  34.64782887210901,
-  36.70809598967594,
-  38.890872965260115,
-  41.20344461410875,
-  43.653528929125486,
-  46.2493028389543,
-  48.999429497718666,
-  51.91308719749314,
-  55.0,
-  58.27047018976124,
-  61.7354126570155,
-  65.40639132514966,
-  69.29565774421802,
-  73.41619197935188,
-  77.78174593052023,
-  82.4068892282175,
-  87.30705785825097,
-  92.4986056779086,
-  97.99885899543733,
-  103.82617439498628,
-  110.0,
-  116.54094037952248,
-  123.47082531403103,
-  130.8127826502993,
-  138.59131548843604,
-  146.8323839587038,
-  155.56349186104046,
-  164.81377845643496,
-  174.61411571650194,
-  184.9972113558172,
-  195.99771799087463,
-  207.65234878997256,
-  220.0,
-  233.08188075904496,
-  246.94165062806206,
-  261.6255653005986,
-  277.1826309768721,
-  293.6647679174076,
-  311.1269837220809,
-  329.6275569128699,
-  349.2282314330039,
-  369.9944227116344,
-  391.99543598174927,
-  415.3046975799451,
-  440.0,
-  466.1637615180899,
-  493.8833012561241,
-  523.2511306011972,
-  554.3652619537442,
-  587.3295358348151,
-  622.2539674441618,
-  659.2551138257398,
-  698.4564628660078,
-  739.9888454232688,
-  783.9908719634985,
-  830.6093951598903,
-  880.0,
-  932.3275230361799,
-  987.7666025122483,
-  1046.5022612023945,
-  1108.7305239074883,
-  1174.6590716696303,
-  1244.5079348883237,
-  1318.5102276514797,
-  1396.9129257320155,
-  1479.9776908465376,
-  1567.981743926997,
-  1661.2187903197805,
-  1760.0,
-  1864.6550460723597,
-  1975.533205024496,
-  2093.004522404789,
-  2217.4610478149766,
-  2349.31814333926,
-  2489.0158697766474,
-  2637.02045530296,
-  2793.825851464031,
-  2959.955381693075,
-  3135.9634878539946,
-  3322.437580639561,
-  3520.0,
-  3729.3100921447194,
-  3951.066410048992
+  16.351597831287414, // C0
+  17.323914436054505, // C#0
+  18.354047994837977, // D0
+  19.445436482630058, // D#0
+  20.601722307054366, // E0
+  21.826764464562746, // F0
+  23.12465141947715,  // F#0
+  24.499714748859326, // G0
+  25.956543598746574, // G#0
+  27.5,               // A0
+  29.13523509488062,  // A#0
+  30.86770632850775,  // B0
+  32.70319566257483,  // C1
+  34.64782887210901,  // C#1
+  36.70809598967594,  // D1
+  38.890872965260115, // D#1
+  41.20344461410875,  // E1
+  43.653528929125486, // F1
+  46.2493028389543,   // F#1
+  48.999429497718666, // G1
+  51.91308719749314,  // G#1
+  55.0,               // A1
+  58.27047018976124,  // A#1
+  61.7354126570155,   // B1
+  65.40639132514966,  // C2
+  69.29565774421802,  // C#2
+  73.41619197935188,  // D2
+  77.78174593052023,  // D#2
+  82.4068892282175,   // E2
+  87.30705785825097,  // F2
+  92.4986056779086,   // F#2
+  97.99885899543733,  // G2
+  103.82617439498628, // G#2
+  110.0,              // A2
+  116.54094037952248, // A#2
+  123.47082531403103, // B2
+  130.8127826502993,  // C3
+  138.59131548843604, // C#3
+  146.8323839587038,  // D3
+  155.56349186104046, // D#3
+  164.81377845643496, // E3
+  174.61411571650194, // F3
+  184.9972113558172,  // F#3
+  195.99771799087463, // G3
+  207.65234878997256, // G#3
+  220.0,              // A3
+  233.08188075904496, // A#3
+  246.94165062806206, // B3
+  261.6255653005986,  // C4
+  277.1826309768721,  // C#4
+  293.6647679174076,  // D4
+  311.1269837220809,  // D#4
+  329.6275569128699,  // E4
+  349.2282314330039,  // F4
+  369.9944227116344,  // F#4
+  391.99543598174927, // G4
+  415.3046975799451,  // G#4
+  440.0,              // A4
+  466.1637615180899,  // A#4
+  493.8833012561241,  // B4
+  523.2511306011972,  // C5
+  554.3652619537442,  // C#5
+  587.3295358348151,  // D5
+  622.2539674441618,  // D#5
+  659.2551138257398,  // E5
+  698.4564628660078,  // F5
+  739.9888454232688,  // F#5
+  783.9908719634985,  // G5
+  830.6093951598903,  // G#5
+  880.0,              // A5
+  932.3275230361799,  // A#5
+  987.7666025122483,  // B5
+  1046.5022612023945, // C5
+  1108.7305239074883, // C#6
+  1174.6590716696303, // D6
+  1244.5079348883237, // D#6
+  1318.5102276514797, // E6
+  1396.9129257320155, // F6
+  1479.9776908465376, // F#6
+  1567.981743926997,  // G6
+  1661.2187903197805, // G#6
+  1760.0,             // A6
+  1864.6550460723597, // A#6
+  1975.533205024496,  // B6
+  2093.004522404789,  // C6
+  2217.4610478149766, // C#7
+  2349.31814333926,   // D7
+  2489.0158697766474, // D#7
+  2637.02045530296,   // E7
+  2793.825851464031,  // F7
+  2959.955381693075,  // F#7
+  3135.9634878539946, // G7
+  3322.437580639561,  // G#7
+  3520.0,             // A7
+  3729.3100921447194, // A#7
+  3951.066410048992   // B7
 };
 
 void sid_transfer(byte address, byte data) {
@@ -469,7 +473,7 @@ void sid_set_filter_mode(byte mode, boolean on) {
   sid_transfer(address, data);
 }
 
-void sid_set_voice_frequency(int voice, word hertz) {
+void sid_set_voice_frequency(int voice, double hertz) {
   word frequency = (hertz / CLOCK_SIGNAL_FACTOR);
   byte loFrequency = lowByte(frequency);
   byte hiFrequency = highByte(frequency);
@@ -626,7 +630,7 @@ void handle_message_voice_detune_change(byte voice, double detune_factor) {
   voice_detune_percents[voice] = detune_factor;
   double semitone_change = (current_pitchbend_amount * midi_pitch_bend_max_semitones) + (voice_detune_percents[voice] * detune_max_semitones);
   double frequency = MIDI_NOTES_TO_FREQUENCIES[notes_playing[voice]] * pow(2, semitone_change / 12.0);
-  sid_set_voice_frequency(voice, (word)frequency);
+  sid_set_voice_frequency(voice, frequency);
 }
 
 void handle_message_note_on(byte note_number, byte velocity) {
@@ -638,7 +642,7 @@ void handle_message_note_on(byte note_number, byte velocity) {
       temp_double = MIDI_NOTES_TO_FREQUENCIES[note_number] * pow(2, temp_double / 12.0);
       if (!volume_modulation_mode_active) {
         sid_set_gate(i, false);
-        sid_set_voice_frequency(i, (word)temp_double);
+        sid_set_voice_frequency(i, temp_double);
         sid_set_gate(i, true);
       }
       notes_playing[i] = note_number;
@@ -670,7 +674,7 @@ void handle_message_note_on(byte note_number, byte velocity) {
     temp_double = MIDI_NOTES_TO_FREQUENCIES[note_number] * pow(2, temp_double / 12.0);
     if (!volume_modulation_mode_active) {
       sid_set_gate(note_voice, false);
-      sid_set_voice_frequency(note_voice, (word)temp_double);
+      sid_set_voice_frequency(note_voice, temp_double);
       sid_set_gate(note_voice, true);
     }
   }
@@ -681,6 +685,7 @@ void handle_message_note_off(byte note_number, byte velocity) {
     if (notes_playing[i] == note_number) {
       notes_playing[i] = NULL;
       note_on_times[i] = NULL;
+      sid_set_voice_frequency(i, 0);
       sid_set_gate(i, false);
       // as a workaround to leakage, iff release is done, we can set the voice
       // frequency to 0 now
@@ -695,7 +700,7 @@ void handle_message_pitchbend_change(word pitchbend) {
     if (notes_playing[i] != NULL) {
       temp_double = (current_pitchbend_amount * midi_pitch_bend_max_semitones) + (voice_detune_percents[i] * detune_max_semitones);
       temp_double = MIDI_NOTES_TO_FREQUENCIES[notes_playing[i]] * pow(2, temp_double / 12.0);
-      sid_set_voice_frequency(i, (word)temp_double);
+      sid_set_voice_frequency(i, temp_double);
     }
   }
 }
