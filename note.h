f0b1342da829a361aff1d24ae1a89b241857c8cc
Patrick Ellis <patrick.j.ellis@gmail.com>
2020-10-20 00:37:21 -0400

    some tidying

---
+++ note.h
@@ -0,0 +1,23 @@
+#ifndef NOTE_H
+#define NOTE_H
+
+#include "util.h"
+
+struct note {
+  byte number;
+  // The midi note number.
+  unsigned long on_time;
+  // The time we got the midi "note on" message. 0 if none so far
+  unsigned long off_time;
+  // The time we got the midi "note off" message. 0 if none so far
+  signed short voiced_by_oscillator;
+  // The oscillator currently sounding the note.
+  // -1 if no oscillator is voicing it (e.g. in a legato run, with 5 notes held
+  // down, only the last one should be voiced. But we need to keep track of all
+  // the "held" notes in case the user releases some of them.)
+};
+
+typedef struct note note;
+
+#endif /* NOTE_H */
+
